d5cc21cdbf44695d4955ce405ac9bb2317005a56
Marcio <marciohamer@gmail.com>
2022-09-23 00:03:37 -0700

    dklfjsdlkfdsklfsdf

--- api/src/index.js
+++ api/src/index.js
@@ -24,18 +24,32 @@ db_connect.connect();
 const port = 5001
 
 
-//str.replace(/[!-#-\--\.]{1}/g, "");
-//replace para regra geral de REMOVER - # .
+const adjustName = (name) => {
+    let nomes = name.split(' ')
+    let ldka = nomes.map((nome)=>{
+        return nome.charAt(0).toUpperCase() + nome.substr(1).toLowerCase();
+    })
+    let txt_name = '';
+    for (let i = 0; i < ldka.length; i++) {
+        txt_name += ldka[i] + ' '; 
+    }
+    return txt_name.trim();
+}
+
 server.get('/oi', (req, res) => {
     aws_connect.query("SELECT * FROM cliente;", (err, results)=>{ 
           if(err){
-            console.log('EROOOOOOOOO', err)
+            console.err('ERROOOOOOOOO', err)
           }
-           console.log(results, 'AAAAAAAAAAAAAAa')
-           console.log(typeof results, 'AAAAAAAAAAAAAAa')    
+        results.forEach(pessoa => {
+             pessoa.nome = adjustName(pessoa.nome.trim())
+             pessoa.cpf = pessoa.cpf.replace(/[!-#-\--\.]{1}/g, "").trim();
+             pessoa.telefone = pessoa.telefone.replace(/[(-)-\--\.]{1}/g, "").trim()
+             pessoa.email = pessoa.email.replace("!","@").replace("#","@").trim().toLowerCase();
+        })
+        console.log(results)
     })
-})
-
+}) 
 
 server.listen(port, () => {
     console.log(`Api rodando => http://localhost:${port}`)
